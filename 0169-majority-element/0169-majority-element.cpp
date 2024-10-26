98b5028344b362b787c7393e931878814c319765
Mohamed ElShab <cinosmoh@gmail.com>
2024-10-26 18:42:31 +0300

    Time: 11 ms (86.8%), Space: 27.2 MB (37.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         map<int, int> frequencies;
-        int most = -1;
+        int mostVal = -1;
         for (int i = 0; i < nums.size(); i++) {
             frequencies[nums[i]]++;
 
-            if (frequencies[most] < frequencies[nums[i]]) most = nums[i];
+            if (frequencies[mostVal] < frequencies[nums[i]]) mostVal = nums[i];
         }
-        return most;
-        // ez solution 1
+        return mostVal;
     }
 };
