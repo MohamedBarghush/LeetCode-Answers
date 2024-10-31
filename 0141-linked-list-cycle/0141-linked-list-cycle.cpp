cd08d61e9fa8bef04dd5cc51602f39a022fe0530
Mohamed ElShab <cinosmoh@gmail.com>
2024-10-31 05:19:10 +0300

    Time: 4 ms (94.63%), Space: 10.8 MB (64.15%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -19,7 +19,7 @@ public:
             if (hare == NULL || hare->next == NULL) // hare reached the end
                 return false;
 
-            tortoise = tortoise->next; // turtle move 1 step
+            tortoise = tortoise->next; // tortoise move 1 step
             hare = hare->next->next; // hare move 2 steps
         }
         return true;
