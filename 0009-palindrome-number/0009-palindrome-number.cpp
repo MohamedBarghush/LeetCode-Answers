8a13de6643945f219628dd4cb9d17d8167f7978a
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-02 00:29:59 +0200

    Time: 4 ms (37.38%), Space: 10.6 MB (13.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string pali = to_string(x);
-        for (int i = 0; i < pali.length()/2; i++) {
-            if (pali[i] != pali[pali.length()-1-i]) return false;
+        string pal = to_string(x);
+        for (int i = 0; i < pal.length()/2; i++) {
+            if (pal[i] != pal[pal.length()-1-i]) return false;
         }
         return true;
     }
