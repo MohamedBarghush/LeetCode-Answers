8b038e7fe795234ebaeafb12309b4438fe71ff61
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-01 03:25:11 +0200

    Time: 9 ms (42.5%), Space: 11.5 MB (54.83%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,8 +3,7 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         int maxSize = max(ransomNote.length(), magazine.length());
         unordered_map<char,int> magLetters;
-        for (char ch : magazine)
-            magLetters[ch]++;
+        for (char ch : magazine) magLetters[ch]++;
         for (char ch : ransomNote) {
             magLetters[ch]--;
             if (magLetters[ch] < 0) return false;
