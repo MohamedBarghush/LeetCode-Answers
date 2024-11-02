3897755f7094f368756a15e5e562aad1e8dfe81a
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-02 10:28:15 +0200

    Time: 0 ms (100%), Space: 10 MB (5.39%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -5,7 +5,10 @@ public:
         while (n != 1) {
             string k = to_string(n);
             n = 0;
-            for (char ch : k) n += (ch-'0')*(ch-'0'); 
+            for (char ch : k)  {
+                int k = ch - '0';
+                n += k*k; 
+            }
             loopDetector[n]++;
             if (loopDetector[n] > 1) return false;
         }
