ede6b0f4f1c993de21f91f4ecd7ea937b53e6aa0
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-03 17:09:39 +0200

    Time: 0 ms (100%), Space: 10.6 MB (5.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,11 +2,6 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         if (digits[digits.size()-1] == 9) {
-            // if (digits.size() == 1) {
-            //     digits[0] = 0;
-            //     digits.insert(digits.begin(), 1);
-            //     return digits;
-            // }
             for (int i = digits.size()-1; i >= 0; i--) {
                 if (digits[i] == 9) {
                     digits[i] = 0;
