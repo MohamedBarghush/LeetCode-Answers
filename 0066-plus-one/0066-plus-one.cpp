1cf6bc20f18c62eeaf84c9ee7f2bfed77a4332a9
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-03 17:10:19 +0200

    Time: 0 ms (100%), Space: 10.5 MB (47.8%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -5,15 +5,13 @@ public:
             for (int i = digits.size()-1; i >= 0; i--) {
                 if (digits[i] == 9) {
                     digits[i] = 0;
-                    if (i == 0) {
-                        digits.insert(digits.begin(),1);
-                        return digits;
-                    }
                     continue;
                 }
                 digits[i]++;
                 return digits;
             }
+            digits.insert(digits.begin(),1);
+            return digits;
         }
         digits[digits.size()-1]++;
         return digits;
