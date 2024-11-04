828b6ca3d11b231c66273b9e60982106b96034ba
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-04 05:53:17 +0200

    Time: 0 ms (100%), Space: 7.8 MB (68.39%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if (s.length() == 0) return true;
+        if (t.length() == 0) return false;
+        int p1 = 0, p2 = 0;
+        while (p1 < s.length() && p2 < t.length()) {
+            if (s[p1] == t[p2]) {
+                p1++;
+                p2++;
+            } else {
+                p2++;
+            }
+            if (p1 == s.length()) return true;
+        }
+        return false;
+    }
+};
