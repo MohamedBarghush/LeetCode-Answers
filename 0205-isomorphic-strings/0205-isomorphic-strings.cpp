ab1b04af024b68645d27ac84596b6dbdffc828d2
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-05 17:04:04 +0200

    Time: 2 ms (59.21%), Space: 9.6 MB (57.51%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> iso;
+        unordered_map<char,int> taken;
+        for (int i = 0; i < s.length(); i++) {
+            if (iso.find(s[i]) != iso.end()) {
+                if (iso[s[i]] == t[i]) {
+                    continue;
+                } else {
+                    return false;
+                }
+            } else {
+                if (taken[t[i]] < 1) {
+                    iso[s[i]] = t[i];
+                    taken[t[i]]++;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
