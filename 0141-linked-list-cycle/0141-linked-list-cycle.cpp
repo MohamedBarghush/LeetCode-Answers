322b0537fb3ebbeef13197ac871b400d493a3d54
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-06 17:01:51 +0200

    Time: 13 ms (15.83%), Space: 10.8 MB (85.47%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,8 +11,8 @@ public:
     bool hasCycle(ListNode *head) {
         if (head == NULL || head->next == NULL) return false;
 
-        ListNode* tortoise = head;
-        ListNode* hare = head->next; 
+        ListNode* tortoise = head; // slow tortoise
+        ListNode* hare = head->next; // quick hare
 
         while (tortoise != hare)
         {
