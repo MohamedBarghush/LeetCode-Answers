bc044622b481ff9a7d89159bcb8326466224d267
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-07 19:22:15 +0200

    Time: 0 ms (100%), Space: 8.3 MB (74.51%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int p1 = a.length()-1;
+        int p2 = b.length()-1;
+        string result;
+        int carry = 0;
+
+        while (p1 >= 0 || p2 >= 0 || carry) {
+            int sum = carry;
+
+            if (p1 >= 0) {
+                sum += a[p1] - '0';
+                p1--;
+            }
+
+            if (p2 >= 0) {
+                sum += b[p2] - '0';
+                p2--;
+            }
+
+            result += (sum%2) + '0';
+            carry = sum / 2;
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
