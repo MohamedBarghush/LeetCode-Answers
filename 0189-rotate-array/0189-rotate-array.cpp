1c7c330e67d39e266e12a39039188318a72db1a2
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-08 07:22:04 +0200

    Time: 0 ms (100%), Space: 27.6 MB (50.67%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -7,10 +7,10 @@ public:
         // Reverse the entire array
         std::reverse(nums.begin(), nums.end());
 
-        // Reverse the first k elements
+        // Reverse the first k
         std::reverse(nums.begin(), nums.begin() + k);
 
-        // Reverse the remaining n - k elements
+        // Reverse the remaining n - k
         std::reverse(nums.begin() + k, nums.end());
     }
 };
