39f71b07dac77353d9fa751d75f6aedc82c8158a
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-10 19:43:58 +0200

    Time: 62 ms (42.71%), Space: 81.3 MB (31.59%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> map; // Use unordered_map to mimic HashMap in Java
+        unordered_map<int, int> map;
         for (int i = 0; i < nums.size(); i++) {
-            if (map.count(nums[i])) { // Equivalent of containsKey in Java
+            if (map.count(nums[i])) {
                 if (i - map[nums[i]] <= k)
                     return true;
                 map[nums[i]] = i;
