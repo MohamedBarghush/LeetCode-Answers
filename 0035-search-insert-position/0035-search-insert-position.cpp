77f3b3c1ec9142dcd79ab754f2d9f4c92c95a437
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-13 19:50:47 +0200

    Time: 0 ms (100%), Space: 12.2 MB (96.93%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     // recursive
-    int binary_search (vector<int>& nums, int target, int min, int max) {
-        int mid = min + (max - min) / 2;
-        if (min > max) return (mid);
+    // int binary_search (vector<int>& nums, int target, int min, int max) {
+    //     int mid = min + (max - min) / 2;
+    //     if (min > max) return (mid);
 
-        if (nums[mid] == target)
-            return mid;
-        else if (nums[mid] > target)
-            return binary_search(nums, target, min, mid-1);
-        else
-            return binary_search(nums, target, mid+1, max);
-    }
+    //     if (nums[mid] == target)
+    //         return mid;
+    //     else if (nums[mid] > target)
+    //         return binary_search(nums, target, min, mid-1);
+    //     else
+    //         return binary_search(nums, target, mid+1, max);
+    // }
     int searchInsert(vector<int>& nums, int target) {
+        // iterative
         int min = 0, max = nums.size()-1;
         int mid;
         while (min <= max) {
