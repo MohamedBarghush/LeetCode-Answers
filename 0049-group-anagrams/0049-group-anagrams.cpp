8325c51450b75034dab870b0072951e8701c90e1
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-16 12:03:23 +0200

    Time: 24 ms (32.77%), Space: 24.1 MB (69.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> letter_groups; // hashmap
+        unordered_map<string, vector<string>> letter_groups;
     
         for (string& str : strs) {
             string sortedStr = str;
