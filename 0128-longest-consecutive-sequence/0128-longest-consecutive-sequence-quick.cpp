5ec7789f1611844a7d7c2dad6a6406824b5ed568
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-17 21:37:26 +0200

    Time: 12 ms (89.89%), Space: 49.6 MB (98.09%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence-quick.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence-quick.cpp
@@ -4,7 +4,7 @@ public:
         int size = nums.size();
         if (size <= 1) return size;
         sort(nums.begin(), nums.end());
-        int count = 1, longest = 1;
+        int count = 1, longest = 1; // for sequence
 
         for (int i = 1; i < size; i++) {
             if (nums[i] == nums[i-1]) continue;
