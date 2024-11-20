dfa54c12876a9785966a00a963ee0427339e5bf5
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-20 18:34:10 +0200

    Time: 4 ms (65.2%), Space: 15.3 MB (10.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> hash;
-        int remain = 0; // rest of the code
+        int remain = 0; 
 
         for (int i = nums.size()-1; i >= 0; i--) {
             remain = target - nums[i];
-            if (hash.count(remain) > 0)
+            if (hash.find(remain) != hash.end())
                 return {hash[remain], i};
             hash[nums[i]] = i;
         }
