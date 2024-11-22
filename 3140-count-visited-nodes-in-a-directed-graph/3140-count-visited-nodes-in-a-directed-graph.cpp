7fdb0b09681ae6b5ffec1e5bea134d1e2f889aa3
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-22 22:44:31 +0200

    Time: 495 ms (25.96%), Space: 325 MB (21.43%) - LeetHub

--- 3140-count-visited-nodes-in-a-directed-graph/3140-count-visited-nodes-in-a-directed-graph.cpp
+++ 3140-count-visited-nodes-in-a-directed-graph/3140-count-visited-nodes-in-a-directed-graph.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     vector<int> countVisitedNodes(vector<int>& edges) {
         int n = edges.size();
-        vector<int> memo(n, -1); // Stores the count for each node
-        vector<int> result(n, 0); // Result for each node
+        vector<int> memo(n, -1);
+        vector<int> result(n, 0);
 
-        // Helper to process a node and calculate visits
         auto calculateVisits = [&](int start) {
             vector<int> path;
             unordered_map<int, int> indexMap;
             int node = start;
 
-            // Traverse the path
             while (memo[node] == -1 && indexMap.find(node) == indexMap.end()) {
                 indexMap[node] = path.size();
                 path.push_back(node);
                 node = edges[node];
             }
 
-            // If node is part of a cycle
             int cycleSize = 0;
             if (memo[node] == -1) {
                 int cycleStartIndex = indexMap[node];
@@ -28,7 +25,6 @@ public:
                 }
             }
 
-            // Propagate memoization for nodes before the cycle
             for (int i = path.size() - 1; i >= 0; i--) {
                 if (memo[path[i]] == -1) {
                     memo[path[i]] = 1 + memo[edges[path[i]]];
@@ -36,7 +32,6 @@ public:
             }
         };
 
-        // Compute visits for each node
         for (int i = 0; i < n; i++) {
             if (memo[i] == -1) {
                 calculateVisits(i);
