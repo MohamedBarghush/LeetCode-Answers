94a9b4bd1fcd4117d15e7e52a21ad347bf0ba616
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-23 20:06:36 +0200

    Time: 4 ms (70.2%), Space: 23 MB (75.28%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,16 +3,16 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> new_intervals;
         sort(intervals.begin(), intervals.end(), 
-        [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
+            [](const vector<int>& a, const vector<int>& b) {
+                return a[0] < b[0];
+            });
         new_intervals.push_back(intervals[0]);
         int n = intervals.size();
         int size = 0;
 
         for (int i = 1; i < n; i++) {
             if (intervals[i][0] <= new_intervals[size][1]) {
-                new_intervals[size][0] = min(intervals[i][0], new_intervals[size][0]);
+                // new_intervals[size][0] = min(intervals[i][0], new_intervals[size][0]);
                 new_intervals[size][1] = max(intervals[i][1], new_intervals[size][1]);
             } else {
                 new_intervals.push_back(intervals[i]);
