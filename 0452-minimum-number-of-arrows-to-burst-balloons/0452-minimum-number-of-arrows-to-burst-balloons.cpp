2fef6201db03f0732e537f6948a3180dc7f9a983
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-23 20:20:21 +0200

    Time: 66 ms (31.91%), Space: 95.6 MB (32.94%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -4,15 +4,15 @@ public:
         sort(points.begin(), points.end()); // sort first
         vector<vector<int>> new_intervals;
         new_intervals.push_back(points[0]);
-        int size = 0;
+        int j = 0;
 
         for (int i = 0; i < points.size(); i++) {
-            if (points[i][0] <= new_intervals[size][1]) {
-                new_intervals[size][0] = max(new_intervals[size][0], points[i][0]);
-                new_intervals[size][1] = min(new_intervals[size][1], points[i][1]);
+            if (points[i][0] <= new_intervals[j][1]) {
+                new_intervals[j][0] = max(new_intervals[j][0], points[i][0]);
+                new_intervals[j][1] = min(new_intervals[j][1], points[i][1]);
             } else {
                 new_intervals.push_back(points[i]);
-                size++;
+                j++;
             }
         }
 
