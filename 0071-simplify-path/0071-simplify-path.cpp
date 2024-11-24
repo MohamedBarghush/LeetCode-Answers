ac7d319a6d769bc312f458eed50319f0dd69319c
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-24 20:16:13 +0200

    Time: 0 ms (100%), Space: 11.8 MB (41.45%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -3,59 +3,31 @@ public:
     string simplifyPath(string path) {
         stack<string> myPath;
         string current;
-        string answer = "/";
-        vector<string> reversedShit;
-        for (int i = 0; i < path.size(); i++) {
-            if (i == path.size()-1) {
-                if (path[i] == '.') {
-                    if (current == ".") {
-                        if (!myPath.empty())
-                            myPath.pop();
-                        break;
-                    } else if (current == "") {
-                        break;
-                    } else {
-                        current += path[i];
-                        myPath.push(current);
-                        break;
-                    }
-                }
-                if (path[i] != '/') {
-                    current += path[i];
-                    myPath.push(current);
-                    break;
-                }
-            }
-            if (path[i] == '/') { 
+        for (char c : path) {
+            if (c == '/') {
                 if (current == "..") {
-                    if (!myPath.empty())
-                        myPath.pop();
-                }
-                else if (current == ".") {
-                    current = "";
-                    continue;
-                }
-                else if (current != "")
+                    if (!myPath.empty()) myPath.pop();
+                } else if (!current.empty() && current != ".") {
                     myPath.push(current);
-                current = "";
+                }
+                current.clear();
             } else {
-                current += path[i];
+                current += c;
             }
         }
 
-        while(!myPath.empty()) {
-            reversedShit.push_back(myPath.top());
+        if (current == "..") {
+            if (!myPath.empty()) myPath.pop();
+        } else if (!current.empty() && current != ".") {
+            myPath.push(current);
+        }
+
+        std::string answer;
+        while (!myPath.empty()) {
+            answer = "/" + myPath.top() + answer;
             myPath.pop();
         }
 
-        for (int i = reversedShit.size()-1; i >= 0; i--) {
-            answer += reversedShit[i] + "/";
-        }
-
-        if (answer.length() > 1) {
-            answer.pop_back();
-        }
-
-        return answer;
+        return answer.empty() ? "/" : answer;
     }
 };
