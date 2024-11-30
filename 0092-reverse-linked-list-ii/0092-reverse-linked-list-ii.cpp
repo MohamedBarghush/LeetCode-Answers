b50aa34151bd4e2b5b487ff1f49728466542a238
Mohamed ElShab <cinosmoh@gmail.com>
2024-11-30 12:21:39 +0200

    Time: 0 ms (100%), Space: 11.2 MB (7.08%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -13,36 +13,26 @@ public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         if (!head || left == right) return head;
 
-        stack<int> values;
-        ListNode* newHead = new ListNode(0);
-        ListNode* it = newHead;
-        int i = 0;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
 
-        while (head != NULL) {
-            if (i == (left-1)) {
-                while (i <= (right-1)) {
-                    values.push(head->val);
-                    head = head->next;
-                    i++;
-                }
+        for (int i = 1; i < left; ++i)
+            prev = prev->next;
 
-                while (!values.empty()) {
-                    ListNode* op = new ListNode(values.top());
-                    values.pop();
-                    it->next = op;
-                    it = it->next;
-                }
+        ListNode* current = prev->next;
+        ListNode* next = nullptr;
+        ListNode* reverseTail = current;
 
-                continue;
-            }
-
-            i++;
-            ListNode* oper = new ListNode(head->val);
-            it->next = oper;
-            it = it->next;
-            head = head->next;
+        for (int i = left; i <= right; i++) {
+            next = current->next;
+            current->next = prev->next;
+            prev->next = current;
+            current = next;
         }
 
-        return newHead->next;
+        reverseTail->next = current;
+
+        return dummy->next;
     }
 };
