1976559203d1f93343538165d2b01db0849047e0
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-01 20:34:23 +0200

    Time: 3 ms (18.65%), Space: 8.7 MB (7.38%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int binary_search (int min, int max, int target) {
+        int mid = min + (max - min) / 2;
+         if (mid <= target / mid && (mid + 1) > target / (mid + 1)) {
+            return mid;
+        } else if (mid > target / mid) {
+            return binary_search(min, mid - 1, target);
+        } else {
+            return binary_search(mid + 1, max, target);
+        }
+    }
+    int mySqrt(int x) {
+        if (x < 2) return x;
+        return binary_search(0, x, x);
+    }
+};
