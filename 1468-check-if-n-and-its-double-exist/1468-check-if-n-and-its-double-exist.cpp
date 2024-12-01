8407e62e7c8daa7009b4643a304ea4df36777c10
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-01 21:04:14 +0200

    Time: 0 ms (100%), Space: 15 MB (10.05%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -3,11 +3,8 @@ public:
     bool checkIfExist(vector<int>& arr) {
         unordered_map<float,int> base;
         for (int i = 0; i < arr.size(); i++) {
-            // cout << arr[i] << endl;
-            if (base.count(arr[i]*2) || base.count(float(arr[i])/2.0f)) {
-                // cout << float(arr[i])/2.0f << endl;
+            if (base.count(arr[i]*2) || base.count(float(arr[i])/2.0f))
                 return true;
-            }
             base[arr[i]]++;
         }
         return false;
