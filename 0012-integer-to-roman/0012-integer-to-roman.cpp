0c4deebbe09c061bdf4989ca89cf61b9dad45f3d
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-02 16:07:05 +0200

    Time: 0 ms (100%), Space: 9.4 MB (87.5%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<int, string>> vals = {
+        static const std::vector<std::pair<int, std::string>> vals = {
             {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
             {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
             {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
         };
 
-        string ans = "";
-        for (pair<int, string>& value : vals) {
-            while (num >= value.first) {
-                ans += value.second;
-                num -= value.first;
+        std::string ans;
+        for (const auto& [value, roman] : vals) {
+            while (num >= value) {
+                ans.append(roman);
+                num -= value;
             }
         }
 
