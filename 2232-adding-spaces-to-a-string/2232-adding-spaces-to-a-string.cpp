fedd0c92fcf9000d7bd90bddccc92cb32905f057
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-03 02:25:13 +0200

    Time: 29 ms (14.16%), Space: 85.5 MB (18.48%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
@@ -3,6 +3,7 @@ public:
     string addSpaces(string s, vector<int>& spaces) {
         string newOne;
         int index = 0;
+
         for (int i = 0; i < s.length(); i++) {
             if (index < spaces.size() && i == spaces[index]) {
                 newOne += ' ';
