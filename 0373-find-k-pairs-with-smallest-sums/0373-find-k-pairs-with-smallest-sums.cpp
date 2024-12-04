ed91501e1098fc54809f042c526f75968ff2314d
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-04 23:47:57 +0200

    Time: 23 ms (99.68%), Space: 132.9 MB (92.73%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -7,14 +7,15 @@ public:
         priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int,int,int>>> minHeap;  // use a complicated priority queue
         
         minHeap.push({nums1[0] + nums2[0], 0, 0});
+        int n = nums1.size(), m = nums2.size();
 
         while (k-- && !minHeap.empty()) {
             auto [sum, i, j] = minHeap.top();
             result.push_back({nums1[i], nums2[j]});
             minHeap.pop();
 
-            if (j == 0 && i+1 < nums1.size()) minHeap.push({nums1[i+1]+nums2[j], i+1, j});
-            if (j + 1 < nums2.size()) minHeap.push({nums1[i] + nums2[j + 1], i, j + 1});
+            if (j == 0 && i+1 < n) minHeap.push({nums1[i+1]+nums2[j], i+1, j});
+            if (j + 1 < m) minHeap.push({nums1[i] + nums2[j + 1], i, j + 1});
         }
 
         return result;
