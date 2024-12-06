ea894bd6c6fdc7a36ec3176078fbfb2822504b25
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-06 16:44:20 +0200

    Time: 0 ms (100%), Space: 128 MB (72.54%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
         vector<int> ban(1e4+1, 0); // following constraints
-        for(int i : banned) ban[i] = 1;
+        for(int i : banned) ban[i] = 1; // fill up the holes
         int count = 0;
         for (int i = 1; i <= n; i++) {
             if (ban[i]) continue;
