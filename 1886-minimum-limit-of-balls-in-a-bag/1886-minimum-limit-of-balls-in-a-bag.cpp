e59271dad15982c9653d9bd688f741e8cede2e50
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-07 16:47:31 +0200

    Time: 27 ms (88.22%), Space: 59.7 MB (28.46%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
-    int binary_search(vector<int>& nums, int left, int right, int max_ops) {
-        // base case
-        if (left == right)
-            return left;
-
-        int mid = left + (right - left) / 2;
-        int ops = 0;
-        for (int n : nums)
-            ops += (n-1) / mid;
-        
-        if (ops <= max_ops) {
-            return binary_search(nums, left, mid, max_ops);
-        } else {
-            return binary_search(nums, mid + 1, right, max_ops);
-        }
-    }
     int minimumSize(vector<int>& nums, int maxOperations) {
-        return binary_search(nums, 1, *max_element(nums.begin(), nums.end()), maxOperations);
+        int low = 1, high = *max_element(nums.begin(), nums.end());
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int ops = 0;
+            for (int n : nums) if ((ops += (n - 1) / mid) > maxOperations) break;
+            ops <= maxOperations ? high = mid : low = mid + 1;
+        }
+        return high;
     }
 };
