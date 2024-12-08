cb7a4b16a5b16a09fcfc861eafa30d8e75ca1d32
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-08 16:40:08 +0200

    Time: 65 ms (69.37%), Space: 124.1 MB (86.56%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
@@ -3,9 +3,7 @@ public:
     int maxTwoEvents(vector<vector<int>>& events) {
         int n = events.size();
 
-        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
+        sort(events.begin(), events.end());
 
         vector<int> suffixMax(n);
         suffixMax[n-1] = events[n-1][2];
