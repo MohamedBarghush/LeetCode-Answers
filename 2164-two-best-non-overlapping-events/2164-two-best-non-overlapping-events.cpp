2eb5d25f17cc609382cdd3f8d70d4571e200dd91
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-08 16:27:03 +0200

    Time: 60 ms (76.58%), Space: 124.1 MB (87.21%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
@@ -11,14 +11,16 @@ public:
         for (int i = n - 2; i >= 0; i--)
             suffixMax[i] = max(events[i][2], suffixMax[i + 1]);
 
-        int maxSum = 0;
+        int maxSum = 0, left = 0, right = 0, mid = 0;
+        int nextEventIndex = -1;
 
         for (int i = 0; i < n; i++) {
-            int left = i + 1, right = n - 1;
-            int nextEventIndex = -1;
+            left = i + 1;
+            right = n - 1;
+            nextEventIndex = -1;
 
             while (left <= right) {
-                int mid = left + (right - left) / 2;
+                mid = left + (right - left) / 2;
                 if (events[mid][0] > events[i][1]) {
                     nextEventIndex = mid;
                     right = mid - 1;
