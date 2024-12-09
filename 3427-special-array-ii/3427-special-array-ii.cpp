8a5d195337fe025748a2294a54155e42e39011c6
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-09 15:45:40 +0200

    Time: 4 ms (87.5%), Space: 126.6 MB (66.56%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -3,22 +3,16 @@ public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
         int q = queries.size();
-        vector<int> invalidIndices;
+        vector<int> prefix(n, 0);
 
-        for (int j = 0; j < n - 1; j++) {
-            if (nums[j] % 2 == nums[j + 1] % 2) {
-                invalidIndices.push_back(j);
-            }
-        }
+        for (int j = 0; j < n - 1; j++)
+            prefix[j + 1] = prefix[j] + (nums[j] % 2 == nums[j + 1] % 2 ? 1 : 0);
 
         vector<bool> answer(q, true);
 
-        for (int i = 0; i < q; i++) {
-            auto it = lower_bound(invalidIndices.begin(), invalidIndices.end(), queries[i][0]);
-
-            if (it != invalidIndices.end() && *it <= queries[i][1] - 1)
+        for (int i = 0; i < q; i++)
+            if (prefix[queries[i][1]] - prefix[queries[i][0]] > 0)
                 answer[i] = false;
-        }
 
         return answer;
     }
