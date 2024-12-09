c34007e472ca65223354a4ade6a565c915ed2800
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-09 15:46:14 +0200

    Time: 3 ms (92.97%), Space: 126.6 MB (66.56%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -4,11 +4,10 @@ public:
         int n = nums.size();
         int q = queries.size();
         vector<int> prefix(n, 0);
+        vector<bool> answer(q, true);
 
         for (int j = 0; j < n - 1; j++)
             prefix[j + 1] = prefix[j] + (nums[j] % 2 == nums[j + 1] % 2 ? 1 : 0);
-
-        vector<bool> answer(q, true);
 
         for (int i = 0; i < q; i++)
             if (prefix[queries[i][1]] - prefix[queries[i][0]] > 0)
