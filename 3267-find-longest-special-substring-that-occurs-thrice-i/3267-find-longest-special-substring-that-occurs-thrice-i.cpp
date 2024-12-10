5095eba631e312b736c819879aa31f590bb71593
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-10 19:43:10 +0200

    Time: 0 ms (100%), Space: 10 MB (92.09%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maximumLength(string s) {
         int l = 0, r = 1, n = s.size();
-        int cnt[26][55] = {};
+        int cnt[26][51] = {};
         int ans = -1;
         while(r <= n) {
             while(r < n && s[r] == s[l]) r++;
