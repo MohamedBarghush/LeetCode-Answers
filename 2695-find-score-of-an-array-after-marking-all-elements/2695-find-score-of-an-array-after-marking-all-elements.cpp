286d6de05e27c45ccc179359ec8a6b95b6a8f88f
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-13 22:16:59 +0200

    Time: 3 ms (99.66%), Space: 92.2 MB (99.69%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    long long findScore(vector<int>& nums) {
-        int n = nums.size();
-        vector<pair<int, int>> elements;
+    long long findScore1(vector<int>& nums) {
+        vector<int> idx(nums.size());
+        iota(idx.begin(), idx.end(), 0);
+        ranges::stable_sort(idx, [&](auto a, auto b) { return nums[a] < nums[b]; });
 
-        for (int i = 0; i < n; i++)
-            elements.emplace_back(nums[i], i);
+        vector<bool> marked(nums.size());
+        long long ans = 0;
 
-        sort(elements.begin(), elements.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-            if (a.first == b.first)
-                return a.second < b.second;
-            return a.first < b.first;
-        });
-
-        vector<bool> marked(n, false);
-        long long score = 0;
-
-        for (const auto& [value, index] : elements) {
-            if (!marked[index]) {
-                score += value;
-
-                marked[index] = true;
-                if (index > 0)
-                    marked[index - 1] = true;
-                if (index < n - 1)
-                    marked[index + 1] = true;
+        for (int i = 0; i < idx.size(); ++i) {
+            if (!marked[idx[i]]) {
+                ans += nums[idx[i]];
+                if (idx[i] - 1 >= 0) {
+                    marked[idx[i] - 1] = true;
+                }
+                if (idx[i] + 1 < nums.size()) {
+                    marked[idx[i] + 1] = true;
+                }
             }
         }
-
-        return score;
+        return ans;
+    }
+    
+    long long findScore(vector<int>& nums) {
+        long long ans = 0;
+        for (int i = 0, n = nums.size(); i < n; i += 2) {
+            int i0 = i;
+            while (i + 1 < n && nums[i] > nums[i + 1])
+                ++i;
+            for (int j = i; j >= i0; j -= 2)
+                ans += nums[j];
+        }
+        return ans;
     }
 };
