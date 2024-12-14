4c97ba608f41120fe44a1b06b4cbc756940cf38b
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-14 14:59:33 +0200

    Time: 91 ms (83.53%), Space: 111.6 MB (94.68%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.cpp
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
-        multiset<int> window;
-        int left = 0;
-        long long totalSubarrays = 0;
+        int right = 0, left = 0;
+        int curMin, curMax;
+        long long windowLen = 0, total = 0;
 
-        for (int right = 0; right < nums.size(); ++right) {
-            window.insert(nums[right]);
+        curMin = curMax = nums[right];
 
-            while (*window.rbegin() - *window.begin() > 2) {
-                window.erase(window.find(nums[left]));
-                ++left;
+        for (right = 0; right < nums.size(); right++) {
+            curMin = min(curMin, nums[right]);
+            curMax = max(curMax, nums[right]);
+
+            if (curMax - curMin > 2) {
+                windowLen = right - left;
+                total += (windowLen * (windowLen + 1) / 2);
+
+                left = right;
+                curMin = curMax = nums[right];
+
+                while (left > 0 && abs(nums[right] - nums[left - 1]) <= 2) {
+                    left--;
+                    curMin = min(curMin, nums[left]);
+                    curMax = max(curMax, nums[left]);
+                }
+
+                if (left < right) {
+                    windowLen = right - left;
+                    total -= (windowLen * (windowLen + 1) / 2);
+                }
             }
-
-            totalSubarrays += (right - left + 1);
         }
 
-        return totalSubarrays;
+        windowLen = right - left;
+        total += (windowLen * (windowLen + 1) / 2);
+
+        return total;
     }
 };
