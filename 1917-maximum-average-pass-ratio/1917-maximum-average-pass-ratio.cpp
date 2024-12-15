7ba3737796eded1f4b679d58c95f71461ba68384
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-15 15:57:09 +0200

    Time: 338 ms (62.39%), Space: 97.6 MB (71.43%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -1,37 +1,54 @@
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        // lambda function to calculate impact
+         // Lambda to calculate impact: the change in pass ratio by adding one student
         auto calculateImpact = [](int pass, long total) {
             double oldRatio = double(pass) / total;
             double newRatio = double(pass + 1) / (total + 1);
             return newRatio - oldRatio;
         };
 
+        // Max-heap to store the classes based on the impact of adding an extra student
         priority_queue<pair<double, int>> maxHeap;
 
+        // Initialize the heap with the impact values of all classes
         for (int i = 0; i < classes.size(); i++) {
-            double impact = calculateImpact(classes[i][0], classes[i][1]);
+            int pass = classes[i][0];
+            int total = classes[i][1];
+            double impact = calculateImpact(pass, total);
             maxHeap.push({impact, i});
         }
 
+        // Assign extra students to the classes with the highest impact
         while (extraStudents > 0) {
-            auto [impact, classer] = maxHeap.top();
+            auto [impact, classIndex] = maxHeap.top();
             maxHeap.pop();
-            classes[classer][0] += 1;
-            classes[classer][1] += 1;
-            impact = calculateImpact(classes[classer][0], classes[classer][1]);
-            maxHeap.push({impact, classer});
+            
+            // Update the class: add one more student
+            classes[classIndex][0] += 1;
+            classes[classIndex][1] += 1;
+
+            // Recalculate the impact after adding a student
+            int pass = classes[classIndex][0];
+            int total = classes[classIndex][1];
+            double newImpact = calculateImpact(pass, total);
+
+            // Push the updated impact back into the heap
+            maxHeap.push({newImpact, classIndex});
+
+            // Decrease the number of extra students
             extraStudents--;
         }
 
+        // Calculate the total pass ratio after all students are assigned
         double totalRatio = 0.0;
         while (!maxHeap.empty()) {
-            auto [_, cls] = maxHeap.top();
+            auto [_, classIndex] = maxHeap.top();
             maxHeap.pop();
-            totalRatio += double(classes[cls][0]) / classes[cls][1];
+            totalRatio += double(classes[classIndex][0]) / classes[classIndex][1];
         }
 
+        // Return the average pass ratio
         return totalRatio / classes.size();
     }
 };
