28749b7f1091ca0d0ade3439653dc334fe767916
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-16 22:42:42 +0200

    Time: 0 ms (100%), Space: 27.7 MB (29.99%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    struct Compare {
+        bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+            // Compare by the number first, then by the index
+            if (a.first == b.first) {
+                return a.second > b.second; // Smaller index has higher priority
+            }
+            return a.first > b.first; // Smaller number has higher priority
+        }
+    };
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> heap;
+
+        for (int i = 0; i < nums.size(); i++) {
+            heap.push({nums[i], i});
+        }
+
+        while (k > 0) {
+            auto [_, idx] = heap.top();
+            heap.pop();
+            nums[idx] *= multiplier;
+            heap.push({nums[idx], idx});
+            k--;
+        }
+
+        return nums;
+    }
+};
