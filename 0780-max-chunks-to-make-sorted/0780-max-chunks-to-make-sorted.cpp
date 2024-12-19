34ced7319e1e1fccf94b0af18a28238873c432ff
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-19 17:23:46 +0200

    Time: 0 ms (100%), Space: 9.5 MB (7.32%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int maxYet = 0;
-        int chunks = 0;
+        stack<int> myStack;
+
         for (int i = 0; i < arr.size(); i++) {
-            maxYet = max(maxYet, arr[i]);
-            if (maxYet == i)
-                chunks++;
+            if (myStack.empty() || arr[i] >= myStack.top()) {
+                myStack.push(arr[i]);
+            } else {
+                int maxVal = myStack.top();
+                myStack.pop();
+                while (!myStack.empty() && myStack.top() > arr[i])
+                    myStack.pop();
+                myStack.push(maxVal);
+            }
         }
-        return chunks;  
+
+        return myStack.size(); 
     }
 };
