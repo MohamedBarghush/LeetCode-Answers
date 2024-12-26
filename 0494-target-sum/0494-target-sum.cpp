6bdbed59f65f66ea7a34e0d725ea2a071ad4c200
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-26 22:18:29 +0200

    Time: 9 ms (66.16%), Space: 25.6 MB (27.8%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int dp (vector<int>& nums, int target, unordered_map<string, int>& memo, int index, int currSum) {
-        if (nums.size() == index) return (currSum == target ? 1 : 0);
+    int dp(vector<int>& nums, int target, vector<vector<int>>& memo, int index, int currSum, int offset) {
+        if (index == nums.size()) return (currSum == target ? 1 : 0);
 
-        string key = to_string(index) + "," + to_string(currSum);
-        if (memo.find(key) != memo.end())
-            return memo[key];
+        if (memo[index][currSum + offset] != -1)
+            return memo[index][currSum + offset];
 
-        int pos = dp(nums, target, memo, index + 1, currSum + nums[index]);
-        int neg = dp(nums, target, memo, index + 1, currSum - nums[index]);
+        int pos = dp(nums, target, memo, index + 1, currSum + nums[index], offset);
+        int neg = dp(nums, target, memo, index + 1, currSum - nums[index], offset);
 
-        memo[key] = pos + neg;
+        memo[index][currSum + offset] = pos + neg;
 
-        return memo[key];
+        return memo[index][currSum + offset];
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<string, int> memo;
-        return dp(nums, target, memo, 0, 0);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int offset = sum;
+        vector<vector<int>> memo(nums.size(), vector<int>(2 * sum + 1, -1));
+        return dp(nums, target, memo, 0, 0, offset);
     }
 };
