a8afee91ba236721ed11b9e69fb8b4b74f0e0f46
Mohamed ElShab <cinosmoh@gmail.com>
2024-12-30 22:13:15 +0200

    Time: 3 ms (96.3%), Space: 11.5 MB (75.63%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -6,18 +6,15 @@ public:
         dp[0] = 1;
 
         for (int i = 1; i <= high; ++i) {
-            if (i >= zero) {
+            if (i >= zero)
                 dp[i] = (dp[i] + dp[i - zero]) % MOD;
-            }
-            if (i >= one) {
+            if (i >= one)
                 dp[i] = (dp[i] + dp[i - one]) % MOD;
-            }
         }
 
         int result = 0;
-        for (int i = low; i <= high; ++i) {
+        for (int i = low; i <= high; ++i)
             result = (result + dp[i]) % MOD;
-        }
         
         return result;
     }
