ef97356350feec0e438bb8b620e85365824dfbab
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-02 23:35:19 +0200

    Time: 1 ms (92.67%), Space: 69.4 MB (67.89%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -7,7 +7,7 @@ public:
         for (int i = 1; i < words.size(); i++)
             prefix[i] = prefix[i - 1] + ((vowels.count(words[i][0]) && vowels.count(words[i].back())) ? 1 : 0);
 
-        vector<int> ans(queries.size());
+        vector<int> ans(queries.size(), 0);
         for (int i = 0; i < queries.size(); i++)
             ans[i] = prefix[queries[i][1]] - (queries[i][0] > 0 ? prefix[queries[i][0]-1] : 0);
 
