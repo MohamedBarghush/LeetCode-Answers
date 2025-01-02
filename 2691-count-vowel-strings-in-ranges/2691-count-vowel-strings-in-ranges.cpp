4cd194601bd81356595afe700d0987879b7e826c
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-02 23:30:52 +0200

    Time: 1 ms (92.67%), Space: 69.5 MB (67.89%) - LeetHub

---
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u'}; // all vowels
+        vector<int> prefix (words.size(), 0);
+        if (vowels.count(words[0][0]) > 0 && vowels.count(words[0].back()) > 0) prefix[0] = 1; else prefix[0] = 0;
+        for (int i = 1; i < words.size(); i++) {
+            if (vowels.count(words[i][0]) > 0 && vowels.count(words[i].back()) > 0)
+                prefix[i] = 1;
+            else
+                prefix[i] = 0;
+            prefix[i] += prefix[i - 1];
+        }
+
+        vector<int> ans(queries.size());
+        for (int i = 0; i < queries.size(); i++)
+            ans[i] = prefix[queries[i][1]] - (queries[i][0] > 0 ? prefix[queries[i][0]-1] : 0);
+
+        return ans;
+    }
+};
