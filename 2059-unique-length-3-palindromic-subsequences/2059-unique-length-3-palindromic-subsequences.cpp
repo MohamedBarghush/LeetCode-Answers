314572f44d4852fd54183c158185774cd150883d
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-04 22:59:46 +0200

    Time: 154 ms (73.99%), Space: 16.1 MB (47.99%) - LeetHub

---
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        vector<int> f(26, -1);
+        vector<int> l(26, -1);
+
+        for (int i = 0; i < s.length(); i++) {
+            int curr = s[i] - 'a';
+            if (f[curr] == -1)
+                f[curr] = i;
+
+            l[curr] = i;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            if (f[i] == -1) continue;
+
+            unordered_set<char> between;
+            for (int j = f[i] + 1; j < l[i]; j++)
+                between.insert(s[j]);
+
+            ans += between.size();
+        }
+
+        return ans;
+    }
+};
