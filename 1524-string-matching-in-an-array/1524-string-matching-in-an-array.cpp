a7173c6b84d14c5d2fd9c7f8150442bbda175467
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-08 00:51:33 +0200

    Time: 0 ms (100%), Space: 11.3 MB (63.14%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -2,33 +2,14 @@ class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
         int n = words.size();
-        sort(words.begin(), words.end(), [](const string &a, const string &b) {
-            return a.length() < b.length();
-        });
-
         vector<string> ans;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                int k = 0;
-                bool flag = true;
-                for (int l = 0; l < words[j].length(); l++) {
-                    if (words[i][k] == words[j][l])
-                        k++;
-                    else {
-                        if (k > 0)
-                            l -= k;
-                        k = 0;
-                    }
-
-                    if (k == words[i].length()) {
-                        ans.push_back(words[i]);
-                        flag = false;
-                        break;
-                    }
+                if (i != j && words[j].find(words[i]) != string::npos) {
+                    ans.push_back(words[i]);
+                    break;
                 }
-                if (!flag) break;
             }
         }
 
