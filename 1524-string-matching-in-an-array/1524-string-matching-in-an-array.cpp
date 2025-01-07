004acdd1134b76524db43650a68807f59e38fcfb
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-08 00:55:24 +0200

    Time: 0 ms (100%), Space: 11.4 MB (63.14%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -4,14 +4,12 @@ public:
         int n = words.size();
         vector<string> ans;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
                 if (i != j && words[j].find(words[i]) != string::npos) {
                     ans.push_back(words[i]);
                     break;
                 }
-            }
-        }
 
         return ans;
     }
