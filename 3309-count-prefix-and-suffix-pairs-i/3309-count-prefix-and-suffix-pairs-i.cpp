0d775b3ffbd1852065ac15305ce77b84932d0eb4
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-08 22:19:46 +0200

    Time: 0 ms (100%), Space: 23.5 MB (77.71%) - LeetHub

---
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPrefixAndSuffix (string str1, string str2) {
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1[i] != str2[i]) return false;
+            if (str1[str1.length()-1-i] != str2[str2.length()-1-i]) return false;
+        }
+        return true;
+    }
+
+    int countPrefixSuffixPairs(vector<string>& words) {
+        int ans = 0;
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = i+1; j < words.size(); j++) {
+                if (isPrefixAndSuffix(words[i], words[j])) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
