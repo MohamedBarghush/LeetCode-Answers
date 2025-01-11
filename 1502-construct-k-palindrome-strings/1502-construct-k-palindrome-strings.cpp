7a375311a26d6d789a212e41f39355900727c2f8
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-11 21:15:22 +0200

    Time: 4 ms (62.59%), Space: 14.8 MB (66.78%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool canConstruct(string s, int k) {
         if (s.length() < k) return false;
+
         int odd = 0;
-        unordered_map<char, int> frequency;
+        int frequency[26] = {};
         for (char c : s)
-            frequency[c]++;
+            frequency[(c - 'a')]++;
 
-        for (const auto& pair : frequency)
-            if (pair.second % 2 == 1) odd++;
-
+        for (int i : frequency)
+            if (i % 2 == 1) odd++;
 
         return odd <= k;
     }
