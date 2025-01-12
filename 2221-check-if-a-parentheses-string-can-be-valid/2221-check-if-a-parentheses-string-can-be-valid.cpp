e9316a82845d8f9ef8f182efe30afe68575c7130
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-12 22:52:23 +0200

    Time: 17 ms (37.19%), Space: 30 MB (70.6%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canBeValid(string s, string locked) {
         int ul = 0, ur = 0, open = 0, close = 0, n = s.length();
-        if(n % 2 == 1) return false;
+        if(n % 2) return false;
 
         for(int i = 0; i<n; i++) {
             if(locked[i] == '0') ul++;
