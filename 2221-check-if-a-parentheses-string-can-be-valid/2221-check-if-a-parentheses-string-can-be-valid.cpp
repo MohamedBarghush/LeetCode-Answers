2c55b7b4abd97e841a8440ee8c720b4607f417a5
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-12 22:53:31 +0200

    Time: 8 ms (90.45%), Space: 30 MB (70.6%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
-        int ul = 0, ur = 0, open = 0, close = 0, n = s.length();
-        if(n % 2) return false; // will never work
-
-        for(int i = 0; i<n; i++) {
-            if(locked[i] == '0') ul++;
-            else if(s[i] == '(') open++;
-            else {
-                if(open > 0) open--;
-                else if(ul > 0) ul--;
-                else return false;
+        if(s.length()%2) return false;
+        int min = 0, max = 0;
+        for(int i=0; i<s.length(); i++){
+            if(locked[i]=='1'){
+                if(s[i]=='('){
+                    min++;
+                    max++;
+                }else{
+                    min--;
+                    max--;
+                }
+            }else{
+                min--;
+                max++;
             }
-
-            if(locked[n-i-1] == '0') ur++;
-            else if(s[n-i-1] == ')') close++;
-            else {
-                if(close > 0) close--;
-                else if(ur > 0) ur--;
-                else return false;
-            }
+            if(min<0) min=0;
+            if(max<0) return false;
         }
-
-        return true;
+        return (min==0);
     }
 };
