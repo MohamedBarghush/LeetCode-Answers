b53e5c2e46724c838a3d98860b9b33bb8f4c3f58
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-12 22:51:06 +0200

    Time: 14 ms (55.78%), Space: 30.1 MB (53.02%) - LeetHub

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int unlocked_bitch = 0, unlocked_whore = 0; 
+        int open = 0, close = 0;
+        int n = s.length();
+        if(n % 2 == 1) return false; // never gonna work
+
+        for(int i = 0; i<n; i++) {
+            if(locked[i] == '0') unlocked_bitch++;
+            else if(s[i] == '(') open++;
+            else {
+                if(open > 0) open--;
+                else if(unlocked_bitch > 0) unlocked_bitch--;
+                else return false;
+            }
+
+            if(locked[n-i-1] == '0') unlocked_whore++;
+            else if(s[n-i-1] == ')') close++;
+            else {
+                if(close > 0) close--;
+                else if(unlocked_whore > 0) unlocked_whore--;
+                else return false;
+            }
+        }
+
+        return true;
+    }
+};
