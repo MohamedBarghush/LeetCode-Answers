fa95001f726ddf9f6871876b4bafbe693caf8ce9
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-13 23:27:33 +0200

    Time: 7 ms (92.69%), Space: 32.1 MB (74.62%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -3,16 +3,15 @@ public:
     int minimumLength(string s) {
         int chars[26] = {};
         int n = s.length();
+        int count = n;
         for (int i = 0; i < n; i++)
             chars[s[i] - 'a']++;
 
         for (int i = 0; i < 26; i++) {
-            while (chars[i] >= 3) chars[i]-=2;
-        }
-
-        int count = 0;
-        for (int i = 0; i < 26; i++) {
-            count += chars[i];
+            while (chars[i] >= 3) {
+                chars[i]-=2;
+                count -= 2;
+            }
         }
 
         return count;
