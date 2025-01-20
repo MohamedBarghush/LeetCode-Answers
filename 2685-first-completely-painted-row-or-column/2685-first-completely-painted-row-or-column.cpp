6937d3d4fe59107d2270f513c64df5320f68ab06
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-20 06:56:36 +0200

    Time: 5 ms (92.68%), Space: 133.9 MB (91.77%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
+        int rows = mat.size();
+        int cols = mat[0].size();
+        int n = arr.size();
+        vector<pair<int, int>> realIndices(n); // <num, row, col>
 
-        vector<pair<int, int>> num_to_coordinates(m*n);
-        for (int r = 0; r < m; r++)
-            for (int c = 0; c < n; c++)
-                num_to_coordinates[mat[r][c]-1] = make_pair(r, c);
+        // preprocess
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                realIndices[mat[i][j]-1] = {i, j};
 
-        vector<int> row_tally(m, 0);
-        vector<int> col_tally(n, 0);
-
-        for (int i = 0; i < arr.size(); i ++) {
-            auto& [r, c] = num_to_coordinates[arr[i]-1];
-            if (++row_tally[r] == n || ++col_tally[c] == m)
-                return i;
+        int* rowFreq = new int[rows]{0};
+        int* columnFreq = new int[cols]{0};
+        // iterate over arr, and increment rows and columns frequencies
+        for (int i = 0; i < n; i++) {
+            auto& [r,c] = realIndices[arr[i]-1];
+            if (++rowFreq[r] == cols) return i;
+            if (++columnFreq[c] == rows) return i;
         }
 
-        return arr.size();
+        return 0;
     }
 };
