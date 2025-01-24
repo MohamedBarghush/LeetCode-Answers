ac203131fa220d47e87dd96d690a7d2c88eb1bcf
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-24 23:43:51 +0200

    Time: 0 ms (100%), Space: 51.7 MB (91.27%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& graph, int* state, int currentIdx) {
+        if (state[currentIdx] == 1) return false;
+        else if (state[currentIdx] == 2) return true;
+
+        state[currentIdx] = 1; // not safe yet
+
+        for (int i : graph[currentIdx])
+            if (!dfs(graph, state, i)) return false;
+
+        state[currentIdx] = 2; // safe
+
+        return true;
+    }
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        int* state = new int[n]{0};
+        vector<int> ans;
+        
+        for (int i = 0; i < n; i++)
+            if (dfs(graph, state, i)) ans.push_back(i);
+
+        return ans;
+    }
+};
