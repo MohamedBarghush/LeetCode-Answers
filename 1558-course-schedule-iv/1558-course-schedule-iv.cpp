840831c75756aeb67622f2fcf6b3787cd896bc85
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-27 22:33:01 +0200

    Time: 18 ms (85.97%), Space: 70.7 MB (58.27%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numC, vector<vector<int>>& pre, vector<vector<int>>& q) {
+        // Reachability Matrix
+        vector<vector<bool>> isReachable(numC, vector<bool>(numC, false));
+        // Adjacency List
+        unordered_map<int, vector<int>> adjList;
+        for (auto edge : pre)
+            adjList[edge[0]].push_back(edge[1]);
+
+
+        // BFS
+        for (int i = 0; i < numC; i++) {
+            queue<int> nodes;
+            nodes.push(i);
+
+            while (!nodes.empty()) {
+                int top = nodes.front();
+                nodes.pop();
+
+                for (int j : adjList[top]) {
+                    if (!isReachable[i][j]) {
+                        isReachable[i][j] = true;
+                        nodes.push(j);
+                    }
+                }
+            }
+        }
+
+        // Deduce answer
+        vector<bool> ans;
+        for (int i = 0; i < q.size(); i++)
+            ans.push_back(isReachable[q[i][0]][q[i][1]]);
+
+        return ans;
+    }
+};
