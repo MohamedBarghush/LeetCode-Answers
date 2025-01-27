76742e7a25730c0402da0c7dd814b1769f0bfff9
Mohamed ElShab <cinosmoh@gmail.com>
2025-01-27 22:36:40 +0200

    Time: 18 ms (85.97%), Space: 70.7 MB (58.27%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.cpp
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int numC, vector<vector<int>>& pre, vector<vector<int>>& q) {
-        // Reachability Matrix
+        // Reachability Matrix (2d vector)
         vector<vector<bool>> isReachable(numC, vector<bool>(numC, false));
-        // Adjacency List
-        unordered_map<int, vector<int>> adjList;
+        // Adjacency List (vector)
+        vector<vector<int>> adjList(numC);
         for (auto edge : pre)
             adjList[edge[0]].push_back(edge[1]);
 
