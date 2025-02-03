2c8424af381fb6bc2e79de4fcbbc715a80b2edbc
Mohamed ElShab <cinosmoh@gmail.com>
2025-02-03 22:02:53 +0200

    Time: 0 ms (100%), Space: 28 MB (27.25%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
-        bool streak = true;
-        int counter = 1, longest = 1;
         int n = nums.size();
+        if (n == 1) return 1;
+        bool greater = nums[1] > nums[0];
+        int counter = 1, longest = 1;
         for (int i = 1; i < n; i++) {
-            if (nums[i] > nums[i-1]) counter++;
-            else counter = 1;
-            longest = max(counter, longest);
-        }
-
-        longest = max(counter, longest);
-
-        counter = 1;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] < nums[i-1]) counter++;
-            else counter = 1;
+            if (nums[i] < nums[i-1]) {
+                if (!greater) {
+                    greater = true;
+                    counter = 2;
+                } else counter++;
+            } else if (nums[i] > nums[i-1]) {
+                if (greater) {
+                    greater = false;
+                    counter = 2;
+                } else counter++;
+            } else { 
+                counter = 1;
+            }
             longest = max(counter, longest);
         }
 
