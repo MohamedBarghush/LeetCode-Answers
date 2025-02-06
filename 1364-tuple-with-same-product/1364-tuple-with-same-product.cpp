f0e265418c86c8eea6db261190b6549e6cf0308d
Mohamed ElShab <cinosmoh@gmail.com>
2025-02-06 14:04:49 +0200

    Time: 306 ms (72.1%), Space: 85 MB (45.45%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int, unsigned char> daMap;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                daMap[nums[i]*nums[j]]++;
+            }
+        }
+
+        int ans = 0;
+        for (auto& [_, key] : daMap) {
+            int count = key;
+            if (count > 1) {
+                ans += 8 * (count * (count - 1) / 2);
+            }
+        }
+
+        return ans;
+    }
+};
