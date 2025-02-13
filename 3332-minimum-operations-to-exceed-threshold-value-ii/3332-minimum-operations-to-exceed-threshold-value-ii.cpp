00008ffc57cb113fe0194e93a5d611b864daadbc
Mohamed ElShab <cinosmoh@gmail.com>
2025-02-13 19:59:35 +0200

    Time: 316 ms (5.04%), Space: 158.8 MB (16.98%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int ans = 0;
-        priority_queue<long, std::vector<long>, std::greater<long>> pq(nums.begin(), nums.end());
-
-        while (pq.size() > 1 && pq.top() < k) {
-            long p1 = pq.top();
-            pq.pop();
-            long p2 = pq.top();
-            pq.pop();
-
-            pq.push(p1 * 2 + p2);
-            ans++;
+        multiset<long> ms(nums.begin(), nums.end());
+        int ops = 0;
+        while (ms.size() > 1 && *ms.begin() < k) {
+            auto it = ms.begin();
+            long p1 = *it;
+            ms.erase(it);
+            it = ms.begin();
+            long p2 = *it;
+            ms.erase(it);
+            
+            ms.insert(2 * p1 + p2);
+            ++ops;
         }
-
-        return ans;
+        return ops;
     }
 };
