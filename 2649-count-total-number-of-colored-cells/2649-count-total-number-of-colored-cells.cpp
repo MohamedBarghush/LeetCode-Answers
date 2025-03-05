c9dd9ef305102d8fa1b70874080dfbeb3dbf3a56
Mohamed ElShab <cinosmoh@gmail.com>
2025-03-05 14:34:17 +0200

    Time: 0 ms (100%), Space: 7.8 MB (61.42%) - LeetHub

---
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        if (n == 1) return 1;
+        long long ans = 1;
+        for (int i = 0; i < n; i++)
+            ans += 4*i;
+        return ans;
+    }
+};
