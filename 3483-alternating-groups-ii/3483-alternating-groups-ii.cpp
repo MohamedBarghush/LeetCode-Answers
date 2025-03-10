f6e066425147c3875870152fb04e3a5bd7e0eed3
Mohamed ElShab <cinosmoh@gmail.com>
2025-03-10 18:54:51 +0200

    Time: 65 ms (78.9%), Space: 102.7 MB (59.49%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
-        // Extend the array to handle circular sequences
-        for (int i = 0; i < k - 1; i++) {
-            colors.push_back(colors[i]);
-        }
+        int n = colors.size();
+        int ans = 0, p1 = 0, p2 = 1;
 
-        int length = colors.size();
-        int result = 0;
-        // Initialize the bounds of the sliding window
-        int left = 0;
-        int right = 1;
-
-        while (right < length) {
-            // Check if the current color is the same as the last one
-            if (colors[right] == colors[right - 1]) {
-                // Pattern breaks, reset window from the current position
-                left = right;
-                right++;
-                continue;
+        // bit optimized
+        while (p1 < n) {
+            int val = p2 % n;
+            if (colors[val] != colors[(val+1) % n]) {
+                if (p2 - p1 < k - 1) {
+                    p2++;
+                } else {
+                    p1++;
+                    p2++;
+                    ans++;
+                }
+            } else {
+                p1 = p2;
+                p2++;
             }
-
-            // Expand the window to the right
-            right++;
-
-            // Skip counting sequence if its length is less than k
-            if (right - left < k) continue;
-
-            // Record a valid sequence and shrink window from the left to search
-            // for more
-            result++;
-            left++;
         }
 
-        return result;
+        return ans;
     }
 };
