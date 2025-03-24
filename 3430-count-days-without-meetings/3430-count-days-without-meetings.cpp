5fca94cf75adac10ffc23e0037ef7cfd20be77f6
Mohamed ElShab <cinosmoh@gmail.com>
2025-03-24 22:27:19 +0200

    Time: 35 ms (97.23%), Space: 126.5 MB (80.45%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -7,10 +7,8 @@ public:
 
         int currEnd = meetings[0][1];
         int freeDays = meetings[0][0]-1;
-        // cout << "Free Days Start: " << freeDays << endl;
         for (int i = 0; i < meetings.size(); i++) {
             if (meetings[i][0] > currEnd) {
-                // cout << "got " << (meetings[i][0] - currEnd) << " free days on index " << i << endl;
                 freeDays += (meetings[i][0] - currEnd)-1;
             }
             currEnd = max(currEnd, meetings[i][1]);
