5c9446c21288137ef1ebb4090bafa001be927645
Mohamed ElShab <cinosmoh@gmail.com>
2025-03-27 22:36:27 +0200

    Time: 4 ms (78.84%), Space: 89.8 MB (86.72%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        unordered_map<int, int> firstMap, secondMap;
-        for (int num : nums) {
-            secondMap[num]++;
+        int majority = nums[0], count = 0;
+        for(auto n: nums){
+            if(n == majority) count++;
+            else count--;
+            if(count == 0) { majority = n; count = 1; }
         }
-
-        int i = 0;
-        for (int num : nums) {
-            firstMap[num]++;
-            secondMap[num]--;
-
-            if (firstMap[num]*2 > (i+1) && secondMap[num]*2 > (nums.size() - i - 1)) return i;
-
-            i++;
+        
+        int majCout = 0;
+        for(auto n: nums) if(n == majority)  majCout++;
+        
+        count = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            if(nums[i] == majority) count++;
+            if(count*2 > (i+1)  && (majCout - count)*2 > (nums.size() - i - 1)) return i;
         }
-
         return -1;
     }
 };
