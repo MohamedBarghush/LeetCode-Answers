af81a562a9cc49d2ed2ab80ac7304601817b1304
Mohamed ElShab <cinosmoh@gmail.com>
2025-04-02 22:12:07 +0200

    Time: 3 ms (31.67%), Space: 21.7 MB (67.16%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cpp
@@ -6,8 +6,7 @@ public:
         for (int i = 0; i < n; i++) {
             for (int j = i+1; j < n; j++) {
                 for (int k = j+1; k < n; k++) {
-                    long long val = nums[i] - nums[j];
-                    val = val * nums[k];
+                    long long val=(long long)(nums[i]-nums[j])*nums[k];
                     // val += ;
                     ans = max(ans, val);
                 }
